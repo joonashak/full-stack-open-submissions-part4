12e57f441d1d42a2306d0adbcf92e7f38f61c032
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-09-03 00:50:12 +0300

    4.5 done

--- utils/listHelper.js
+++ utils/listHelper.js
@@ -2,7 +2,17 @@ const dummy = blogs => 1;
 
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, blog) => {
+    if (favorite.likes > blog.likes) return favorite;
+    return blog;
+  };
+
+  return blogs.reduce(reducer, blogs[0]);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
