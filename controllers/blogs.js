dd612a011929dba16e54e70787d8e4a524101d85
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-09-02 16:10:24 +0300

    4.2 done

---
+++ controllers/blogs.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const Blog = require('../models/blog');
+
+
+router.get('/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => response.json(blogs));
+});
+
+router.post('/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then(result => response.status(201).json(result));
+});
+
+module.exports = router;
