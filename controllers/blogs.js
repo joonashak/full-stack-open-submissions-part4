6a06cd342b1ea60e70c8e6ae4c4ce0a0ebeb87db
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-09-09 15:57:49 +0300

    5.9

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -51,7 +51,7 @@ router.delete('/blogs/:id', jwt({ secret: 'dfjuhg78h4bhk5g0' }), async (request,
 
   const { user } = await Blog.findOne({ _id });
 
-  if (user.toString() !== request.user.id) {
+  if (user && user.toString() !== request.user.id) {
     return response.status(400).send('You cannot delete this blog.');
   }
 
